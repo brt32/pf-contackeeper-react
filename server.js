8c2bbe7f92116891826e916aa2d98d336ddd201c
Bartek <bartlomiej.proczkowski@gmail.com>
2021-11-08 22:31:56 +0100

    Changes in server.js

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const path = require("path");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
+// Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API" })
-);
-
-// Define routes
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-// Server static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
@@ -29,4 +26,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
